14b09ce9488a5fe64ead83259dd1812942819348
tom5079 <7948651+tom5079@users.noreply.github.com>
2022-08-24 06:30:43 +0000

    responsive text

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,21 @@ function App() {
     <div className='bg-gray-900'>
       <Background/>
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex-column text-center">
-        <h1 className="text-[25vw] leading-none font-wendy text-white">
+        <h1 className="text-[25vw] md:text-9xl leading-none font-wendy text-white">
           DDR
         </h1>
-        <h2 className="text-[15vw] leading-none font-wendy text-white">
+        <h2 className="text-[15vw] md:text-7xl leading-none font-wendy text-white">
           queue
         </h2>
-        <p className="text-[7vw] font-wendy text-white">
+        <p className="text-[7vw] md:text-5xl font-wendy text-white">
           3939 teams in queue
         </p>
-        <p className="text-[7vw] font-wendy text-white">
+        <p className="text-[7vw] md:text-5xl font-wendy text-white">
           estimated wait time: 100 years
         </p>
       </div>
       <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-1/4">
-        <p className="text-[10vw] font-wendy text-white">
+        <p className="text-[10vw] md:text-7xl font-wendy text-white">
           join queue
         </p>
       </div>
