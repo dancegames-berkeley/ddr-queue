37efff5a800e0144a1bdfca7f57b129810a08bb3
tom5079 <7948651+tom5079@users.noreply.github.com>
2022-08-31 22:47:12 -0700

    --wip-- [skip ci]

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,43 @@
-import { useState } from 'react'
+import { state, useState, useEffect } from 'react'
 import Background from './Background'
 import logo from '/logo.png'
 import "./index.css"
 
 function App() {
+  const [queueSize, setQueueSize] = useState(-1);
+
+  useEffect(() => {
+    const socket = new WebSocket('wss://75dz4fc17b.execute-api.us-west-1.amazonaws.com/production');
+
+    const queueInfo = () => socket.send(JSON.stringify({ action: 'queueInfo' }));
+
+    socket.onopen = (event) => queueInfo();
+
+    socket.onmessage = (event) => {
+      const msg = JSON.parse(event.data);
+
+      switch(msg.action) {
+        case "queueInfo":
+          setQueueSize(msg.queueSize);
+          break;
+      }
+    };
+
+    const intervalId = setInterval(() => queueInfo(), 10000);
+
+    return () => {
+      socket.close();
+      clearInterval(intervalId);
+    }
+  }, []);
+  
   return (
-    <div className='bg-gray-900 h-full w-full'>
+    <div className="bg-gray-900 h-full w-full">
       <Background/>
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex-column text-center">
-        <img src={logo}/>
+        <img src={logo} className="max-w-[50%] translate-x-1/2"/>
         <p className="text-[5vw] sm:text-3xl font-misolight text-white">
-          3939 teams in queue
+          {queueSize} teams in queue
         </p>
         <p className="text-[5vw] sm:text-3xl font-misolight text-white">
           Estimated wait time: 100 years
